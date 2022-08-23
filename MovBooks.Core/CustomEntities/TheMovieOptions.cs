d2a63d04526b8f9b36eedb84c118f48260d3d798
christian741 <christian.xd.417@gmail.com>
2022-08-22 22:30:23 -0500

    Add changes to Enviroment variables

---
+++ MovBooks.Core/CustomEntities/TheMovieOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovBooks.Core.CustomEntities
+{
+    public class TheMovieOptions
+    {
+        public string Api { get; set; }
+        public string ApiKey { get; set; }
+        public string Language { get; set; }
+        public bool Enable { get; set; }
+        public bool EnableTrackingGenders { get; set; }
+    }
+}
