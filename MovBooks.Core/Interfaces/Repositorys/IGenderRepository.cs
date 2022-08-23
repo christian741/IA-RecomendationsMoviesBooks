e9b90282aaaf8d170de4e62dac291c07886011d3
christian741 <christian.xd.417@gmail.com>
2022-08-22 22:29:18 -0500

    Add changes to Save Genders Moph type to books and movies

--- MovBooks.Core/Interfaces/Repositorys/IGenderRepository.cs
+++ MovBooks.Core/Interfaces/Repositorys/IGenderRepository.cs
@@ -2,11 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MovBooks.Core.Interfaces.Repositorys
 {
     public interface IGenderRepository : IRepository<Gender>
     {
+        Task<Gender> FindByName(string name);
 
+        Task saveGenderBooks(Gender gender, Book book);
+
+        Task saveGenderMovies(Gender gender, Movie movie);
     }
 }
